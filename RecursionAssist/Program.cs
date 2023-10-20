238f1cfc7d221904aa265b952274d518ea250d51
narramuni <nmspavan@gmail.com>
2023-10-20 21:23:12 +0530

    Add project files.

---
+++ RecursionAssist/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionAssist
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+
+        }
+        static void runApp()
+        {
+
+           
+            {
+                string[] students = { "Sai", "Guru", "Tharun", "Venkat", "Kumar" };
+
+                Student studentPrinter = new Student();
+                studentPrinter.PrintStudentsViaRecursion(students, 0);
+                Console.ReadLine(); 
+            }
+
+        }
+       
+        
+    }
+}
---
+++ RecursionAssist/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionAssist
+{
+    public class Student
+    {
+        public void PrintStudentsViaRecursion(string[] students, int index)
+        {
+            if (index < students.Length)
+            {
+                Console.WriteLine(students[index]);
+                PrintStudentsViaRecursion(students, index + 1);
+            }
+        }
+    }
+}
